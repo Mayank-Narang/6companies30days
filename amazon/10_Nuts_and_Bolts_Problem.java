7ed73fc0d57d8b65c40703b5de01da6d1a165e10
Mayank-Narang <137385758+Mayank-Narang@users.noreply.github.com>
2025-01-09 17:31:32 +0530

    what is this question saying

---
+++ amazon/10_Nuts_and_Bolts_Problem.java
@@ -0,0 +1,8 @@
+// I was not able to understand that is this solution even correct or i might have understood it properly. and thanks to gfg comment section that gives the answer rather than discussing about the question
+
+class Solution {
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        Arrays.sort(nuts);
+        Arrays.sort(bolts);
+    }
+}
